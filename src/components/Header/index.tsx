7a8bab002cd9a1b5ff4ea8369be8aa064082dd12
kissinger156 <severo.e.carlos@gmail.com>
2021-11-02 23:39:30 -0300

    started implementation of button language switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,37 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import ReactFlagsSelect from 'react-flags-select'
 import { getLanguage } from '../../helpers/getLanguage'
 import ActiveLink from './Link'
 import { HeaderWrapper } from './style'
 
 export const Header: React.FC = () => {
+  const router = useRouter()
   const translate = getLanguage()
+
+  function handleChangeLocale(locale: string) {
+    router.push(router.pathname, null, {
+      locale: locale === 'US' ? 'en' : 'pt-BR'
+    })
+  }
+
   return (
     <HeaderWrapper>
       <ActiveLink href="/">{translate.navbar.home}</ActiveLink>
       <ActiveLink href="/about">{translate.navbar.about}</ActiveLink>
       <ActiveLink href="/projects">{translate.navbar.projects}</ActiveLink>
       <ActiveLink href="/contact">{translate.navbar.contact}</ActiveLink>
+      <ReactFlagsSelect
+        countries={['US', 'BR']}
+        customLabels={{
+          US: 'English',
+          BR: 'Português'
+        }}
+        showOptionLabel={false}
+        showSelectedLabel={false}
+        selected={router.locale === 'en' ? 'US' : 'BR'}
+        onSelect={code => handleChangeLocale(code)}
+      />
     </HeaderWrapper>
   )
 }
