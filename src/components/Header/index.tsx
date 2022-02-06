791ac8cd6b913dacee60d63517fbe5ab13e63d2d
kissinger156 <severo.e.carlos@gmail.com>
2022-02-06 15:30:19 -0300

    Feature - Add hamburger menu to mobile devices

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactFlagsSelect from 'react-flags-select'
 import { getLanguage } from '../../helpers/getLanguage'
 import { ToggleThemeButton } from '../ToggleThemeButton'
@@ -7,8 +7,15 @@ import ActiveLink from './Link'
 import { HeaderWrapper } from './style'
 
 export const Header: React.FC = () => {
+  const [activeMenuMobile, setActiveMenuMobile] = useState('')
+  const [bxActive, setBxActive] = useState('')
   const router = useRouter()
   const translate = getLanguage()
+
+  useEffect(() => {
+    setActiveMenuMobile('')
+    setBxActive('')
+  }, [router.pathname])
 
   function handleChangeLocale(locale: string) {
     router.push(router.pathname, null, {
@@ -16,27 +23,49 @@ export const Header: React.FC = () => {
     })
   }
 
+  const MenuContent = () => {
+    return (
+      <>
+        <ActiveLink href="/">{translate.navbar.home}</ActiveLink>
+        <ActiveLink href="/about">{translate.navbar.about}</ActiveLink>
+        <ActiveLink href="/projects">{translate.navbar.projects}</ActiveLink>
+        <ActiveLink href="/contact">{translate.navbar.contact}</ActiveLink>
+        <ReactFlagsSelect
+          countries={['US', 'BR']}
+          customLabels={{
+            US: 'English',
+            BR: 'Português'
+          }}
+          showOptionLabel={false}
+          showSelectedLabel={false}
+          selected={router.locale === 'en' ? 'US' : 'BR'}
+          onSelect={code => handleChangeLocale(code)}
+        />
+        <ToggleThemeButton />
+      </>
+    )
+  }
+
   return (
     <HeaderWrapper>
-      <ActiveLink href="/">{translate.navbar.home}</ActiveLink>
-      <ActiveLink href="/about">{translate.navbar.about}</ActiveLink>
-      <ActiveLink href="/projects">{translate.navbar.projects}</ActiveLink>
-      <ActiveLink href="/contact">{translate.navbar.contact}</ActiveLink>
-      {
-        // TODO add a mobile menu
-      }
-      <ReactFlagsSelect
-        countries={['US', 'BR']}
-        customLabels={{
-          US: 'English',
-          BR: 'Português'
-        }}
-        showOptionLabel={false}
-        showSelectedLabel={false}
-        selected={router.locale === 'en' ? 'US' : 'BR'}
-        onSelect={code => handleChangeLocale(code)}
-      />
-      <ToggleThemeButton />
+      <nav className="navbar-desktop">
+        <MenuContent />
+      </nav>
+      <nav className="navbar-mobile">
+        <div
+          className={`bx ${bxActive}`}
+          onClick={() => {
+            setActiveMenuMobile(prev =>
+              prev === '' ? 'active-menu-mobile' : ''
+            )
+            setBxActive(prev => (prev === '' ? 'bx-active' : ''))
+          }}
+        ></div>
+      </nav>
+
+      <nav className={`menu-mobile ${activeMenuMobile}`}>
+        <MenuContent />
+      </nav>
     </HeaderWrapper>
   )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,68 +1,186 @@
 import styled from 'styled-components'
 
 export const HeaderWrapper = styled.header`
-  width: 100%;
-  z-index: 10;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding: 10px 0;
-  background: var(--color-bg-primary);
+  .navbar-desktop {
+    width: 100%;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    background: var(--color-bg-primary);
 
-  a {
-    font-weight: 400;
-    font-size: 18px;
-    color: var(--color-text-primary);
-    position: relative;
-    text-decoration: none;
-    margin: 0 15px;
+    a {
+      font-weight: 400;
+      font-size: 18px;
+      color: var(--color-text-primary);
+      position: relative;
+      text-decoration: none;
+      margin: 0 15px;
 
-    transition: all 0.2s;
-
-    &::after {
-      content: '';
-      display: block;
-      position: absolute;
-      top: -30px;
-      left: 45%;
-      width: 5px;
-      height: 0px;
-      border-radius: 50px;
-      background: var(--color-blue-3);
-
-      transition: all 0.5s;
-    }
-
-    &:hover {
-      color: var(--color-blue-1);
+      transition: all 0.2s;
 
       &::after {
-        height: 30px;
+        content: '';
+        display: block;
+        position: absolute;
+        top: -30px;
+        left: 45%;
+        width: 5px;
+        height: 0px;
+        border-radius: 50px;
+        background: var(--color-blue-3);
+
         transition: all 0.5s;
       }
+
+      &:hover {
+        color: var(--color-blue-1);
+
+        &::after {
+          height: 30px;
+          transition: all 0.5s;
+        }
+      }
+    }
+
+    .active {
+      color: var(--color-blue-2);
+      font-weight: 500;
+      position: relative;
+
+      transition: all 0.2s;
+
+      &::after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: -30px;
+        left: 45%;
+        width: 5px;
+        height: 30px;
+        border-radius: 100px;
+        background: var(--color-blue-3);
+      }
+    }
+
+    @media (max-width: 996px) {
+      display: none;
     }
   }
 
-  .active {
-    color: var(--color-blue-2);
-    font-weight: 500;
-    position: relative;
+  .navbar-mobile {
+    display: none;
 
-    transition: all 0.2s;
+    .bx {
+      width: 40px;
+      height: 40px;
 
-    &::after {
-      content: '';
-      display: block;
-      position: absolute;
-      top: -30px;
-      left: 45%;
-      width: 5px;
-      height: 30px;
-      border-radius: 100px;
-      background: var(--color-blue-3);
+      position: fixed;
+      right: 10px;
+
+      z-index: 999;
+
+      &::after,
+      &::before {
+        content: '';
+        width: 75%;
+        height: 2px;
+
+        position: absolute;
+        background: var(--color-text-primary);
+
+        transition: all 0.5s;
+      }
+
+      &::after {
+        top: 35%;
+        left: 12%;
+      }
+
+      &::before {
+        top: 57%;
+        left: 12%;
+      }
     }
+
+    .bx-active {
+      width: 36px;
+      height: 36px;
+
+      position: fixed;
+      right: 10px;
+
+      z-index: 999;
+
+      &::after,
+      &::before {
+        content: '';
+        position: absolute;
+        top: 45%;
+
+        background: $white;
+
+        transition: all 0.5s;
+      }
+
+      &::after {
+        transform: rotate(-135deg);
+      }
+
+      &::before {
+        transform: rotate(315deg);
+      }
+    }
+
+    @media (max-width: 996px) {
+      display: flex;
+    }
+  }
+
+  .menu-mobile {
+    width: 100vw;
+    height: 100vh;
+
+    background: var(--color-bg-primary);
+
+    position: fixed;
+    top: 0;
+
+    z-index: -10;
+    opacity: 0;
+
+    display: none;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    transition: all 0.5s;
+
+    a {
+      font-weight: 400;
+      font-size: 22px;
+      color: var(--color-text-primary);
+      position: relative;
+      text-decoration: none;
+    }
+
+    div,
+    a,
+    button {
+      margin: 20px 0;
+    }
+
+    @media (max-width: 996px) {
+      display: flex;
+    }
+  }
+
+  .active-menu-mobile {
+    opacity: 1;
+    z-index: 998;
   }
 `
